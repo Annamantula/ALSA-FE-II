2b1266edce5c8d3eded86ab7afc2c61924bcda23
thompscott <tho.sco.lisd@gmail.com>
2022-08-17 12:23:41 -0500

    added login and register functions

---
+++ src/components/Register.js
@@ -0,0 +1,56 @@
+import { useState } from "react"
+import { userRegister } from "../api/loginindex";
+
+function Register() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [message, setMessage] = useState("");
+    const [token, setToken] = useState(localStorage.getItem("token") ? localStorage.getItem("token"):"");
+
+    const submitHandler = async (event) => {
+        event.preventDefault();
+        const result = await userRegister(email, password);
+        setMessage(result.message);
+        if (result.token) {
+            setToken(result.tokan);
+            localStorage.setItem("token", result.tokan);
+            localStorage.setItem("email", email);
+        }
+    }
+
+    return(
+        (token ? 
+        <div>
+            <h2>Register</h2>
+            <h3>Logged in as {email}</h3>
+            <button onClick={() => {
+                localStorage.removeItem("token");
+                setToken("");
+            }}>Log Out</button>
+        </div>
+        :<div>
+            <form className="register" onSubmit={submitHandler}>
+                <h2>Register</h2>
+                <fieldset>
+                    <label htmlFor="email">
+                        Email
+                    </label>
+                    <input minLength={1} id="email" type="text" placeholder="email" value={email} onChange={(event) => {setEmail(event.target.value)}}/>
+                </fieldset>
+                <fieldset>
+                <label htmlFor="password">
+                        Password
+                    </label>
+                    <input minLength={1} id="password" type="text" placeholder="Password" value={password} onChange={(event) => {setPassword(event.target.value)}}/>
+                </fieldset>
+                <fieldset>
+                    <button type="submit">Register</button>
+                    <p>{message}</p>
+                </fieldset>
+            </form>
+        </div>
+        )
+    )
+}
+
+export default Register
